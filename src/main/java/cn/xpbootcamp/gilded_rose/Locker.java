08b7eaca01c018651d329c2b423e1738fb5981e4
hfguan <hfguan@thoughtworks.com>
2020-03-25 20:43:00 +0800

    add unlock test and impl

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -1,9 +1,24 @@
 package cn.xpbootcamp.gilded_rose;
 
+import java.util.Map;
+
 public class Locker {
     private Integer size;
+    private Map<Ticket, Bag> ticketBagMap;
+
+    public Locker(int i) {
+        this.size=i;
+    }
 
     public Ticket lock(Bag bag) {
         return new Ticket();
+    }
+
+    public Bag unlock(Ticket ticket) throws Exception {
+        if (size == 19) {
+            throw new Exception("柜子已满");
+        }
+        size-=1;
+       return ticketBagMap.get(ticket);
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -8,8 +8,18 @@ public class LockerTest {
 
     @Test
     public void should_return_ticket_when_lock_bag_given_locker_is_not_full() {
-        Locker locker = new Locker();
+        Locker locker = new Locker(16);
         Ticket ticket = locker.lock(new Bag());
         assertThat(ticket).isNotNull();
+    }
+
+    @Test
+    public void should_return_exception_when_unlock_bag_given_locker_is_full() {
+        Locker locker = new Locker(19);
+        try {
+            locker.unlock(new Ticket());
+        } catch (Exception e) {
+            assertThat(e.getMessage()).isEqualTo("柜子已满");
+        }
     }
 }
