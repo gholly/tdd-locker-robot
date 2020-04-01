08074df8e010cb263c075946aa4a36134639dc5b
hfguan <hfguan@thoughtworks.com>
2020-04-01 22:20:57 +0800

    add store bag unit test  and impl when locker is full

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerRobotTest.java
@@ -1,9 +1,11 @@
 package cn.xpbootcamp.gilded_rose;
 
+import cn.xpbootcamp.gilded_rose.exception.LockerFullException;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LockerRobotTest {
 
@@ -17,5 +19,17 @@ public class LockerRobotTest {
         Ticket ticket = lockerRobot.storeBag(new Bag("my bag"));
         //then
         assertThat(ticket).isNotNull();
-     }
+    }
+
+    @Test
+    public void should_throw_locker_full_exception_when_store_bag_given_locker_robot_is_full() {
+        //given
+        Locker locker = new Locker(20, 0);
+        Locker locker01 = new Locker(20, 0);
+        LockerRobot lockerRobot = new LockerRobot(Lists.newArrayList(locker, locker01));
+        //when then
+        assertThrows(LockerFullException.class, () -> lockerRobot.storeBag(new Bag("my bag")));
+    }
+
+
 }
