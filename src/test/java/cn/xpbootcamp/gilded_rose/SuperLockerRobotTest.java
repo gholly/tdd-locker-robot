82c478ac826b909394e99e23902c936ff9978619
hfguan <hfguan@thoughtworks.com>
2020-04-14 21:22:15 +0800

    add store bag unit test

--- src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.java
@@ -24,6 +24,39 @@ public class SuperLockerRobotTest {
         assertThat(secondLocker.take(ticket)).isEqualTo(bag);
     }
 
+    @Test
+    public void should_return_ticket_and_store_1st_locker_when_store_bag_given_super_robot_have_1st_locker_empty_capacity_rate_is_3_rate_4_and_2nd_locker_empty_capacity_rate_is_3_rate_4() {
+        //given
+        Locker firstLocker = new Locker(4);
+        Locker secondLocker = new Locker(4);
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(Lists.newArrayList(firstLocker, secondLocker));
+        firstLocker.store(new Bag());
+        secondLocker.store(new Bag());
+
+        //when
+        Bag bag = new Bag();
+        Ticket ticket = superLockerRobot.store(bag);
+        //then
+        assertThat(ticket).isNotNull();
+        assertThat(firstLocker.take(ticket)).isEqualTo(bag);
+    }
+
+    @Test
+    public void should_return_ticket_and_store_1st_locker_when_store_bag_given_super_robot_have_1st_locker_empty_capacity_rate_is_1_and_2nd_locker_empty_capacity_rate_is_3_rate_4() {
+        //given
+        Locker firstLocker = new Locker(4);
+        Locker secondLocker = new Locker(4);
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(Lists.newArrayList(firstLocker, secondLocker));
+        secondLocker.store(new Bag());
+
+        //when
+        Bag bag = new Bag();
+        Ticket ticket = superLockerRobot.store(bag);
+        //then
+        assertThat(ticket).isNotNull();
+        assertThat(firstLocker.take(ticket)).isEqualTo(bag);
+    }
+
 
 
 
